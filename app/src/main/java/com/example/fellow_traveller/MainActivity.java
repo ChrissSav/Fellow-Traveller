35409d60b7a677899a8234cf7174230be5123f19
ChrissSav <dai17023@uom.edu.gr>
2020-03-18 19:15:11 +0200

    Add Login Activity and update some styles

--- app/src/main/java/com/example/fellow_traveller/MainActivity.java
+++ app/src/main/java/com/example/fellow_traveller/MainActivity.java
@@ -2,13 +2,30 @@ package com.example.fellow_traveller;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button btn_login;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btn_login= findViewById(R.id.MainActivity_button_login);
+
+
+        btn_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(intent);
+                //finish();
+            }
+        });
     }
 }
