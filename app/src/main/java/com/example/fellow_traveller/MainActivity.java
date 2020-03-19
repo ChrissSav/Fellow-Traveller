999dfb299d49a91f5238930f97f49faaca6aae97
ChrissSav <dai17023@uom.edu.gr>
2020-03-19 17:53:33 +0200

    Add register Activity and update some styles

--- app/src/main/java/com/example/fellow_traveller/MainActivity.java
+++ app/src/main/java/com/example/fellow_traveller/MainActivity.java
@@ -10,19 +10,29 @@ import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Button btn_login;
+    private Button btn_login, btn_register;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        btn_login= findViewById(R.id.MainActivity_button_login);
-
+        btn_login = findViewById(R.id.MainActivity_button_login);
+        btn_register = findViewById(R.id.MainActivity_button_register);
 
         btn_login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(intent);
+                //finish();
+            }
+        });
+
+        btn_register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, RegisterActivity.class);
                 startActivity(intent);
                 //finish();
             }
