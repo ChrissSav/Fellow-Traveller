2f5e7918451764465a7ac6c75e88ccc2649931c1
Spuros <spurakos98@gmail.com>
2020-03-25 21:39:20 +0200

    Update User Account

--- app/src/main/java/com/example/fellow_traveller/LoginActivity.java
+++ app/src/main/java/com/example/fellow_traveller/LoginActivity.java
@@ -2,13 +2,27 @@ package com.example.fellow_traveller;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class LoginActivity extends AppCompatActivity {
-
+private Button loginButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        loginButton = findViewById(R.id.LoginActivity_button_login);
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(LoginActivity.this, HomeActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
