aaa98a4d568bfaa11ee59a20562482b4fb8abbbc
Spuros <spurakos98@gmail.com>
2020-03-28 02:12:11 +0200

    Settings Created

--- app/src/main/java/com/example/fellow_traveller/HomeFragments/AccountFragment.java
+++ app/src/main/java/com/example/fellow_traveller/HomeFragments/AccountFragment.java
@@ -1,6 +1,7 @@
 package com.example.fellow_traveller.HomeFragments;
 
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -8,13 +9,19 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageButton;
 
+import com.example.fellow_traveller.LoginActivity;
+import com.example.fellow_traveller.MainActivity;
 import com.example.fellow_traveller.R;
+import com.example.fellow_traveller.SettingsActivity;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class AccountFragment extends Fragment {
+    private ImageButton settingsButton;
 
 
     public AccountFragment() {
@@ -26,7 +33,19 @@ public class AccountFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_account, container, false);
+        View view = inflater.inflate(R.layout.fragment_account, container, false);
+        settingsButton =  view.findViewById(R.id.settings_button);
+
+        settingsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getActivity(), SettingsActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        return view;
+
     }
 
 }
