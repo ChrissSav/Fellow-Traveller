91af9bcb2f3815396dea636c47bdb36b7c0e7b85
Spuros <spurakos98@gmail.com>
2020-03-28 22:47:43 +0200

    Chat Updated

--- app/src/main/java/com/example/fellow_traveller/ChatConversationActivity.java
+++ app/src/main/java/com/example/fellow_traveller/ChatConversationActivity.java
@@ -1,14 +1,28 @@
 package com.example.fellow_traveller;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class ChatConversationActivity extends AppCompatActivity {
+    private EditText writeEdtText;
+    private ConstraintLayout borderOfEdtText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat_conversation);
+        writeEdtText = findViewById(R.id.write_et_chat);
+        borderOfEdtText = findViewById(R.id.border_of_et);
+
+        writeEdtText.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View view, boolean b) {
+                borderOfEdtText.setBackgroundResource( R.drawable.write_message_et_black_chat);
+            }
+        });
     }
 }
