9b99c466af16a2721b46f3a9b1e1a66c89be4a27
SpurosRantoglou <38006349+SpurosRantoglou@users.noreply.github.com>
2020-03-30 00:24:11 +0300

    Search Filter Updated

--- app/src/main/java/com/example/fellow_traveller/FiltersActivity.java
+++ app/src/main/java/com/example/fellow_traveller/FiltersActivity.java
@@ -3,12 +3,47 @@ package com.example.fellow_traveller;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
 public class FiltersActivity extends AppCompatActivity {
-
+    private SeekBar rangeBar;
+    private TextView rangeBarTV;
+    private Button sortByButton, dateButton, timeButton, seatsButton, ratingButton, resetButton, applyButton;
+    private ImageButton closeButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_filters);
+
+        rangeBar = findViewById(R.id.slider_filters);
+        rangeBarTV = findViewById(R.id.range_km_tv);
+        sortByButton = findViewById(R.id.sort_by_button);
+        dateButton = findViewById(R.id.date_button);
+        timeButton = findViewById(R.id.time_button);
+        seatsButton = findViewById(R.id.seats_button);
+        resetButton = findViewById(R.id.reset_button);
+        ratingButton = findViewById(R.id.rate_button);
+        applyButton = findViewById(R.id.apply_button);
+        closeButton = findViewById(R.id.close_button_filters);
+
+        rangeBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                rangeBarTV.setText(progress + " χλμ");
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
     }
 }
