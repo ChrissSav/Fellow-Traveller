70daa94ccb3ddcdc88e40dfcf0ec15b5e912c01b
ChrissSav <dai17023@uom.edu.gr>
2020-04-12 16:00:45 +0300

    Update layouts

--- app/src/main/java/com/example/fellow_traveller/NewOffer/NewOfferStage3Fragment.java
+++ app/src/main/java/com/example/fellow_traveller/NewOffer/NewOfferStage3Fragment.java
@@ -1,6 +1,7 @@
 package com.example.fellow_traveller.NewOffer;
 
 
+import android.app.AlertDialog;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -9,13 +10,13 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
-import android.widget.CompoundButton;
 import android.widget.ImageButton;
 import android.widget.Switch;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.fellow_traveller.R;
+
 
 /**
  * A simple {@link Fragment} subclass.
@@ -25,11 +26,13 @@ public class NewOfferStage3Fragment extends Fragment {
 
     private View view;
     private ImageButton seats_increase, seats_decrease, bags_increase, bags_decrease;
-    private TextView seats_tv,bags_tv;
+    private TextView seats_tv, bags_tv;
     private String num_of_seats = "0";
     private String num_of_bags = "0";
     private Switch pet_switch;
     private Boolean pets = false;
+    private Button select_seats;
+
     public NewOfferStage3Fragment() {
         // Required empty public constructor
     }
@@ -40,74 +43,62 @@ public class NewOfferStage3Fragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         view = inflater.inflate(R.layout.fragment_new_offer_stage3, container, false);
-//        seats_increase = view.findViewById(R.id.NewOfferStage3Fragment_button_plus_seats);
-//        seats_decrease = view.findViewById(R.id.NewOfferStage3Fragment_button_minus_seats);
-//
-//        bags_increase = view.findViewById(R.id.NewOfferStage3Fragment_button_plus_bags);
-//        bags_decrease = view.findViewById(R.id.NewOfferStage3Fragment_button_minus_bags);
-//
-//        seats_tv = view.findViewById(R.id.NewOfferStage3Fragment_TextView_seats);
-//        bags_tv = view.findViewById(R.id.NewOfferStage3Fragment_TextView_bags);
-//
-//        pet_switch = view.findViewById(R.id.NewOfferStage3Fragment_switch_pets);
-//        seats_tv.setText(num_of_seats);
-//        bags_tv.setText(num_of_bags);
-//
-//        if(pets){
-//            pet_switch.setChecked(true);
-//            pet_switch.setText("Allow");
-//        }
-//
-//        seats_increase.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                Increase(seats_tv);
-//            }
-//        });
-//
-//        seats_decrease.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                Decrease(seats_tv);
-//            }
-//        });
-//
-//        bags_increase.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                Increase(bags_tv);
-//            }
-//        });
-//
-//        bags_increase.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                Increase(bags_tv);
-//            }
-//        });
-//
-//        bags_decrease.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                Decrease(bags_tv);
-//            }
-//        });
-//
-//
-//
-//        pet_switch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-//            @Override
-//            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-//                if(isChecked){
-//                    pet_switch.setText("Allow");
-//                }else{
-//                    pet_switch.setText("Not Allow");
-//                }
-//            }
-//        });
+        select_seats = view.findViewById(R.id.NewOfferStage3Fragment_button_seat);
 
+        select_seats.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                openDialog();
+            }
+        });
         return view;
     }
+
+    public void openDialog() {
+        AlertDialog.Builder mBuilder = new AlertDialog.Builder(getActivity());
+        View mView = getLayoutInflater().inflate(R.layout.number_choose, null);
+        Button button = mView.findViewById(R.id.choose_num_button);
+        ImageButton increase = mView.findViewById(R.id.choose_num_imageButton_plus);
+        ImageButton dicrease = mView.findViewById(R.id.choose_num_imageButton_minus);
+        final TextView textView_number = mView.findViewById(R.id.choose_num_textView_number);
+        TextView textView_title = mView.findViewById(R.id.choose_num_textView_title);
+        if(!select_seats.getText().equals("Θέσεις ...")){
+            textView_number.setText(select_seats.getText().toString());
+        }
+
+        textView_title.setText("Καθόρισε τον αριθμό των θέσεων");
+        mBuilder.setView(mView);
+
+        final AlertDialog dialog = mBuilder.create();
+        dialog.show();
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                dialog.dismiss();
+                select_seats.setText(textView_number.getText().toString());
+
+            }
+        });
+
+        increase.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Increase(textView_number);
+            }
+        });
+
+        dicrease.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Decrease(textView_number);
+            }
+        });
+
+    }
+
 
     public String toString() {
         return "NewOfferStage3Fragment";
@@ -117,21 +108,22 @@ public class NewOfferStage3Fragment extends Fragment {
         return 3;
     }
 
-    public void Increase(TextView textView){
+    public void Increase(TextView textView) {
         int current_num = Integer.parseInt(textView.getText().toString());
-        textView.setText((current_num+1)+"");
+        textView.setText((current_num + 1) + "");
 
     }
-    public void Decrease(TextView textView){
+
+    public void Decrease(TextView textView) {
         int current_num = Integer.parseInt(textView.getText().toString());
-        if(current_num > 0)
-            textView.setText((current_num-1)+"");
+        if (current_num > 0)
+            textView.setText((current_num - 1) + "");
     }
 
     @Override
     public void onDestroy() {
         // Log.i("textInputLayout_pass_1", "onDestroy");
- //       num_of_seats = seats_tv.getText().toString();
+        //       num_of_seats = seats_tv.getText().toString();
 //        num_of_bags = bags_tv.getText().toString();
 //        if(pet_switch.isChecked())
 //            pets =true;
@@ -139,8 +131,9 @@ public class NewOfferStage3Fragment extends Fragment {
 //            pets = false;
         super.onDestroy();
     }
-    public boolean isOk(){
-        if(Integer.parseInt(seats_tv.getText().toString())<1){
+
+    public boolean isOk() {
+        if (Integer.parseInt(seats_tv.getText().toString()) < 1) {
             Toast.makeText(getActivity(), "Ο αριθμός των θέσων πρέπει να ειναι τουλάστον 1!", Toast.LENGTH_SHORT).show();
             return false;
         }
@@ -150,10 +143,12 @@ public class NewOfferStage3Fragment extends Fragment {
     public String getNum_of_seats() {
         return seats_tv.getText().toString();
     }
+
     public String getNum_of_bags() {
         return bags_tv.getText().toString();
     }
+
     public String getPet() {
-        return pet_switch.isChecked()+"";
+        return pet_switch.isChecked() + "";
     }
 }
--- app/src/main/java/com/example/fellow_traveller/NewOffer/NewOfferStage5Fragment.java
+++ app/src/main/java/com/example/fellow_traveller/NewOffer/NewOfferStage5Fragment.java
@@ -34,25 +34,8 @@ public class NewOfferStage5Fragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         view = inflater.inflate(R.layout.fragment_new_offer_stage5, container, false);
-        editText =view.findViewById(R.id.NewOfferStage5Fragment_editText_price);
 
-        editText.addTextChangedListener(new TextWatcher() {
-            @Override
-            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-                Log.i("addTextChangedListener","beforeTextChanged" + s);
 
-            }
-
-            @Override
-            public void onTextChanged(CharSequence s, int start, int before, int count) {
-                Log.i("addTextChangedListener","onTextChanged " +s );
-            }
-
-            @Override
-            public void afterTextChanged(Editable s) {
-                Log.i("addTextChangedListener","afterTextChanged");
-            }
-        });
 
 
         return  view;
