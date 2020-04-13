8a5aaa81c6c8e62e8e625adf6994b27df15efe76
Spuros <spurakos98@gmail.com>
2020-04-13 03:10:58 +0300

    Display only the last message

--- app/src/main/java/com/example/fellow_traveller/Chat/ConversationAdapter.java
+++ app/src/main/java/com/example/fellow_traveller/Chat/ConversationAdapter.java
@@ -7,6 +7,12 @@ import android.widget.TextView;
 
 import com.example.fellow_traveller.R;
 import com.example.fellow_traveller.SearchAndBook.SearchResultsAdapter;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
 
 import java.security.Timestamp;
 import java.text.DateFormat;
@@ -21,6 +27,7 @@ import androidx.recyclerview.widget.RecyclerView;
 public class ConversationAdapter extends RecyclerView.Adapter<ConversationAdapter.ConversationViewHolder>{
     private ArrayList<ConversationItem> conversationList;
     private ConversationAdapter.OnItemClickListener mListener;
+    String theLastMessage;
 
     public interface OnItemClickListener{
         void onItemClick(int position);
@@ -72,7 +79,10 @@ public class ConversationAdapter extends RecyclerView.Adapter<ConversationAdapte
     public void onBindViewHolder(@NonNull ConversationViewHolder holder, int position) {
         ConversationItem currentItem = conversationList.get(position);
         holder.userName.setText(currentItem.getTripName());
-        holder.description.setText(currentItem.getDescription());
+
+        //Call the method to get the last message and we parse the Trip Id to retrieve the messages and the TextView to set the text
+        lastMessage(Integer.toString(currentItem.getTripId()), holder.description);
+
 
 
         Date currentDate = new Date(currentItem.getDate()*1000);
@@ -107,5 +117,44 @@ public class ConversationAdapter extends RecyclerView.Adapter<ConversationAdapte
     @Override
     public int getItemCount() {
         return conversationList.size();
+    }
+
+    //Check for the last message
+    private void lastMessage(String groupId, final TextView last_message ) {
+
+        theLastMessage = "default";
+        DatabaseReference reference = FirebaseDatabase.getInstance().getReference("Messages").child(groupId);
+        reference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                for(DataSnapshot snapshot: dataSnapshot.getChildren()){
+                    MessageItem item = snapshot.getValue(MessageItem.class);
+                    theLastMessage = item.getText();
+                }
+
+
+                switch (theLastMessage){
+                    case "default":
+                        last_message.setText("Δεν υπάρχει κάποιο μήνυμα");
+                        break;
+
+                    default:
+                        last_message.setText(theLastMessage);
+                        break;
+                }
+                theLastMessage = "default";
+            }
+
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
+
+
+
+
     }
 }
