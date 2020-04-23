4fcb767d3d1e2144942b31e838715bca7fe35a63
Spuros <spurakos98@gmail.com>
2020-04-24 00:45:58 +0300

    Write a review

--- app/src/main/java/com/example/fellow_traveller/Reviews/ReviewsActivity.java
+++ app/src/main/java/com/example/fellow_traveller/Reviews/ReviewsActivity.java
@@ -4,7 +4,10 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.example.fellow_traveller.Chat.MessageItem;
 import com.example.fellow_traveller.Chat.MessagesAdapter;
@@ -18,11 +21,14 @@ public class ReviewsActivity extends AppCompatActivity {
     private RecyclerView.Adapter mAdapter;
     private LinearLayoutManager mLayoutManager;
     private ArrayList<ReviewItem> reviewsList = new ArrayList<>();
+    private Button writeReviewButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_reviews);
+
+        writeReviewButton = findViewById(R.id.reviews_write_review_button);
 
         reviewsList.add(new ReviewItem("Γιασεμής Μαραμένος", "22 Ιαν 2020", "3,5", "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam"));
         reviewsList.add(new ReviewItem("Tyler Joseph" , "22 Ιαν 2020", "3,5", "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam"));
@@ -37,5 +43,14 @@ public class ReviewsActivity extends AppCompatActivity {
         mAdapter = new ReviewsAdapter(reviewsList);
         mRecyclerView.setLayoutManager(mLayoutManager);
         mRecyclerView.setAdapter(mAdapter);
+
+
+        writeReviewButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(ReviewsActivity.this, WriteReviewActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/fellow_traveller/Reviews/WriteReviewActivity.java
@@ -0,0 +1,16 @@
+package com.example.fellow_traveller.Reviews;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.fellow_traveller.R;
+
+public class WriteReviewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_write_review);
+    }
+}
