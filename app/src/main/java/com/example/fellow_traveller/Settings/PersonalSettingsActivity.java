02e586566aa3965af2103a9053937bcbc002aa30
Aldi <iamaldi@users.noreply.github.com>
2020-04-28 00:39:38 +0300

    Update PersonalSettingsActivity.java
    cleaning up a bit

--- app/src/main/java/com/example/fellow_traveller/Settings/PersonalSettingsActivity.java
+++ app/src/main/java/com/example/fellow_traveller/Settings/PersonalSettingsActivity.java
@@ -1,32 +1,26 @@
 package com.example.fellow_traveller.Settings;
 
-import androidx.appcompat.app.AppCompatActivity;
-
-import de.hdodenhof.circleimageview.CircleImageView;
-
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.net.Uri;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.Toast;
 
+import androidx.appcompat.app.AppCompatActivity;
+
 import com.example.fellow_traveller.ClientAPI.Callbacks.UserAuthCallback;
 import com.example.fellow_traveller.ClientAPI.FellowTravellerAPI;
 import com.example.fellow_traveller.ClientAPI.Models.UserAuthModel;
-import com.example.fellow_traveller.HomeFragments.HomeActivity;
-import com.example.fellow_traveller.LoginActivity;
-import com.example.fellow_traveller.MainActivity;
 import com.example.fellow_traveller.Models.GlobalClass;
 import com.example.fellow_traveller.R;
 import com.google.gson.Gson;
 import com.theartofdev.edmodo.cropper.CropImage;
 
-import java.util.regex.Pattern;
+import de.hdodenhof.circleimageview.CircleImageView;
 
 import static com.example.fellow_traveller.Util.InputValidation.isValidPhone;
 
@@ -35,7 +29,7 @@ public class PersonalSettingsActivity extends AppCompatActivity {
     private ImageButton imageButtonClose, imageButtonUpdate;
     private CircleImageView profilePicture;
     private Uri mImageUri;
-    private EditText editTextFirstName, editTextLastName, editTextAboutMe, editTextNumber;
+    private EditText firstNameEditText, lastNameEditText, aboutMeEditText, phoneNumberEditText;
     private GlobalClass globalClass;
 
 
@@ -51,10 +45,10 @@ public class PersonalSettingsActivity extends AppCompatActivity {
         imageButtonUpdate = findViewById(R.id.PersonalSettingsActivity_button_update);
 
 
-        editTextFirstName = findViewById(R.id.PersonalSettingsActivity_editText_first_name);
-        editTextLastName = findViewById(R.id.PersonalSettingsActivity_editText_last_name);
-        editTextAboutMe = findViewById(R.id.PersonalSettingsActivity_editText_about_me);
-        editTextNumber = findViewById(R.id.PersonalSettingsActivity_editText_phone);
+        firstNameEditText = findViewById(R.id.PersonalSettingsActivity_editText_first_name);
+        lastNameEditText = findViewById(R.id.PersonalSettingsActivity_editText_last_name);
+        aboutMeEditText = findViewById(R.id.PersonalSettingsActivity_editText_about_me);
+        phoneNumberEditText = findViewById(R.id.PersonalSettingsActivity_editText_phone);
 
 
         changeImageButton.setOnClickListener(new View.OnClickListener() {
@@ -76,52 +70,43 @@ public class PersonalSettingsActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 if (namesValidation() && phoneValidation()) {
-                    updateUser(editTextFirstName.getText().toString(), editTextLastName.getText().toString(),
-                            editTextAboutMe.getText().toString(), editTextNumber.getText().toString());
+                    updateUser(firstNameEditText.getText().toString(), lastNameEditText.getText().toString(),
+                            aboutMeEditText.getText().toString(), phoneNumberEditText.getText().toString());
                 }
-
             }
         });
     }
 
 
     public void onChooseFile(View v) {
-
         CropImage.activity().start(PersonalSettingsActivity.this);
     }
 
     public boolean phoneValidation() {
-        String phone = editTextNumber.getText().toString();
+        String phone = phoneNumberEditText.getText().toString();
         if (!isValidPhone(phone)) {
             Toast.makeText(this, getResources().getString(R.string.INVALID_PHONE_FORMAT), Toast.LENGTH_SHORT).show();
             return false;
-
         }
         return true;
 
     }
 
     public boolean namesValidation() {
-        if (editTextFirstName.getText().length() < 3 || editTextLastName.getText().length() < 3) {
+        if (firstNameEditText.getText().length() < 3 || lastNameEditText.getText().length() < 3) {
             Toast.makeText(this, "Ελέγξετε τα πεδία.", Toast.LENGTH_SHORT).show();
             return false;
-
         }
         return true;
-
     }
 
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-
-
+        // TODO get profile pic from user device...
         if (requestCode == CropImage.CROP_IMAGE_ACTIVITY_REQUEST_CODE) {
-
-
             CropImage.ActivityResult result = CropImage.getActivityResult(data);
-
             if (resultCode == RESULT_OK) {
                 mImageUri = result.getUri();
                 profilePicture.setImageURI(mImageUri);
@@ -133,27 +118,25 @@ public class PersonalSettingsActivity extends AppCompatActivity {
     }
 
     public void fillFields() {
-
-        new FellowTravellerAPI(globalClass).getUserInfo(new UserAuthCallback() {
+        FellowTravellerAPI.getUserInfo(new UserAuthCallback() {
             @Override
             public void onSuccess(UserAuthModel user) {
-                editTextFirstName.setText(user.getName());
-                editTextLastName.setText(user.getSurname());
-                editTextAboutMe.setText(user.getAboutMe());
-                editTextNumber.setText(user.getPhone());
+                firstNameEditText.setText(user.getName());
+                lastNameEditText.setText(user.getSurname());
+                aboutMeEditText.setText(user.getAboutMe());
+                phoneNumberEditText.setText(user.getPhone());
             }
 
             @Override
             public void onFailure(String errorMsg) {
-
+                // TODO use resource strings to fill the fields when data from server is unavailable
             }
         });
     }
 
 
-    public void updateUser(String name, String lastName, String aboutMe, String phone) {
-
-        new FellowTravellerAPI(globalClass).updateUserInfo(name, lastName, "", aboutMe, phone, new UserAuthCallback() {
+    public void updateUser(String firstName, String lastName, String aboutMe, String phoneNumber) {
+        FellowTravellerAPI.updateUserInfo(firstName, lastName, "", aboutMe, phoneNumber, new UserAuthCallback() {
             @Override
             public void onSuccess(UserAuthModel user) {
                 user.setSessionId(globalClass.getCurrentUser().getSessionId());
@@ -167,7 +150,6 @@ public class PersonalSettingsActivity extends AppCompatActivity {
             @Override
             public void onFailure(String errorMsg) {
                 Toast.makeText(PersonalSettingsActivity.this, errorMsg, Toast.LENGTH_SHORT).show();
-
             }
         });
     }
