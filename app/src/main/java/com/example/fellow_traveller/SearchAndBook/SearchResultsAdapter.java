ed384b94007a891023aa0ffa7df31b21eede1829
Aldi <iamaldi@users.noreply.github.com>
2020-05-04 00:44:26 +0300

    Update SearchResultsAdapter.java

--- app/src/main/java/com/example/fellow_traveller/SearchAndBook/SearchResultsAdapter.java
+++ app/src/main/java/com/example/fellow_traveller/SearchAndBook/SearchResultsAdapter.java
@@ -1,71 +1,36 @@
 package com.example.fellow_traveller.SearchAndBook;
 
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.fellow_traveller.ClientAPI.Models.TripModel;
 import com.example.fellow_traveller.R;
 
 import java.util.ArrayList;
 
-import androidx.annotation.NonNull;
-import androidx.recyclerview.widget.RecyclerView;
-
-public class SearchResultsAdapter extends RecyclerView.Adapter<SearchResultsAdapter.SearchResultsViewHolder>{
+public class SearchResultsAdapter extends RecyclerView.Adapter<SearchResultsAdapter.SearchResultsViewHolder> {
     private ArrayList<TripModel> searchResultList;
     private OnItemClickListener mListener;
 
-    public interface OnItemClickListener{
-        void onItemClick(int position);
-    }
-
-    public void setOnItemClickListener(OnItemClickListener listener){
-        mListener = listener;
-    }
-    public static class SearchResultsViewHolder extends RecyclerView.ViewHolder{
-        public TextView userName, rate, review, from, to, date, time;
-
-
-
-
-        public SearchResultsViewHolder(@NonNull View itemView, final OnItemClickListener listener) {
-            super(itemView);
-
-            userName = itemView.findViewById(R.id.name_search_item);
-            rate = itemView.findViewById(R.id.rate_search_item);
-            review = itemView.findViewById(R.id.review_search_item);
-            from = itemView.findViewById(R.id.from_search_item);
-            to = itemView.findViewById(R.id.to_search_item);
-            date = itemView.findViewById(R.id.date_search_item);
-            time = itemView.findViewById(R.id.time_search_item);
-
-            itemView.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
-                    if(listener != null){
-                        int position = getAdapterPosition();
-                        if(position != RecyclerView.NO_POSITION){
-                            listener.onItemClick(position);
-                        }
-                    }
-                }
-            });
-
-        }
-    }
-    public SearchResultsAdapter(ArrayList<TripModel> searchList){
+    public SearchResultsAdapter(ArrayList<TripModel> searchList) {
         searchResultList = searchList;
 
+    }
+
+    public void setOnItemClickListener(OnItemClickListener listener) {
+        mListener = listener;
     }
 
     @NonNull
     @Override
     public SearchResultsViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.search_result_item, parent, false);
-        SearchResultsViewHolder evh = new SearchResultsViewHolder (v, mListener);
+        SearchResultsViewHolder evh = new SearchResultsViewHolder(v, mListener);
         return evh;
     }
 
@@ -86,5 +51,39 @@ public class SearchResultsAdapter extends RecyclerView.Adapter<SearchResultsAdap
     @Override
     public int getItemCount() {
         return searchResultList.size();
+    }
+
+    public interface OnItemClickListener {
+        void onItemClick(int position);
+    }
+
+    public static class SearchResultsViewHolder extends RecyclerView.ViewHolder {
+        public TextView userName, rate, review, from, to, date, time;
+
+
+        public SearchResultsViewHolder(@NonNull View itemView, final OnItemClickListener listener) {
+            super(itemView);
+
+            userName = itemView.findViewById(R.id.name_search_item);
+            rate = itemView.findViewById(R.id.rate_search_item);
+            review = itemView.findViewById(R.id.review_search_item);
+            from = itemView.findViewById(R.id.from_search_item);
+            to = itemView.findViewById(R.id.to_search_item);
+            date = itemView.findViewById(R.id.date_search_item);
+            time = itemView.findViewById(R.id.time_search_item);
+
+            itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    if (listener != null) {
+                        int position = getAdapterPosition();
+                        if (position != RecyclerView.NO_POSITION) {
+                            listener.onItemClick(position);
+                        }
+                    }
+                }
+            });
+
+        }
     }
 }
