e8ba546b28c8b7b9289182ba7198a43ca6131c6b
Aldi <iamaldi@users.noreply.github.com>
2020-05-04 15:11:52 +0300

    Update Search2Activity.java

--- app/src/main/java/com/example/fellow_traveller/SearchAndBook/Search2Activity.java
+++ app/src/main/java/com/example/fellow_traveller/SearchAndBook/Search2Activity.java
@@ -1,10 +1,5 @@
 package com.example.fellow_traveller.SearchAndBook;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.constraintlayout.widget.ConstraintLayout;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.Editable;
@@ -16,16 +11,17 @@ import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
-import android.widget.Toast;
 
-import com.example.fellow_traveller.ClientAPI.RetrofitAPIEndpoints;
-
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.fellow_traveller.Models.GlobalClass;
 import com.example.fellow_traveller.PlacesAPI.CallBack.PlaceApiCallBack;
 import com.example.fellow_traveller.PlacesAPI.Models.PlaceAPiModel;
-import com.example.fellow_traveller.PlacesAPI.PlaceAdapter;
 import com.example.fellow_traveller.PlacesAPI.Models.PredictionsModel;
+import com.example.fellow_traveller.PlacesAPI.PlaceAdapter;
 import com.example.fellow_traveller.PlacesAPI.PlaceApiConnection;
 import com.example.fellow_traveller.R;
 
@@ -41,8 +37,6 @@ public class Search2Activity extends AppCompatActivity {
     private PlaceAdapter mAdapter;
     private RecyclerView.LayoutManager mLayoutManager;
     private ArrayList<PredictionsModel> places_list;
-    private RetrofitAPIEndpoints retrofitService;
-
     private GlobalClass globalClass;
 
     @Override
@@ -50,9 +44,7 @@ public class Search2Activity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search2);
 
-
         globalClass = (GlobalClass) getApplicationContext();
-
 
         destinationAutoComplete = findViewById(R.id.autocomplete_search_destination);
         backButton = findViewById(R.id.back_button_search);
@@ -107,8 +99,8 @@ public class Search2Activity extends AppCompatActivity {
 
 
         //destinationAutoComplete.setAdapter(new PlaceAutocompleteAdapter(Search2Activity.this, android.R.layout.simple_list_item_1));
-        Intent intent = getIntent();
-        final String fromString = intent.getExtras().getString("FromPlace");
+        final Intent intent = getIntent();
+//        final String fromString = intent.getExtras().getString("FromPlace");
 
         destinationAutoComplete.setOnEditorActionListener(new TextView.OnEditorActionListener() {
             @Override
@@ -117,7 +109,7 @@ public class Search2Activity extends AppCompatActivity {
                     if (!destinationAutoComplete.getText().toString().trim().isEmpty()) {
                         Intent mainIntent = new Intent(Search2Activity.this, SearchResultsActivity.class);
                         mainIntent.putExtra("ToPlace", destinationAutoComplete.getText().toString());
-                        mainIntent.putExtra("FromPlace", fromString);
+                        mainIntent.putExtra("FromPlace", intent.getExtras().getString("FromPlace"));
                         startActivity(mainIntent);
                     } else {
                         destinationAutoComplete.setError("Δεν έχετε επιλέξει τον προορισμό σας");
@@ -133,21 +125,19 @@ public class Search2Activity extends AppCompatActivity {
 
     public void GetPlaces(String input) {
 
-        new PlaceApiConnection(globalClass).getPlaces(input,new PlaceApiCallBack() {
+        new PlaceApiConnection(globalClass).getPlaces(input, new PlaceApiCallBack() {
             @Override
             public void onSuccess(PlaceAPiModel placeAPiModel) {
                 places_list = placeAPiModel.getPredictions();
                 buildRecyclerView();
             }
-
-
-
+            
             @Override
             public void onFailure(String errorMsg) {
 
             }
         });
-            
+
     }
 
     public void buildRecyclerView() {
