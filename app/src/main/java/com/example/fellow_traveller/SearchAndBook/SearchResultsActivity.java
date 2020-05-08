1152c2c94eee774f38e48e1a9e1bc6184696cb78
Spuros <spurakos98@gmail.com>
2020-05-08 15:25:34 +0300

    Layout fixes

--- app/src/main/java/com/example/fellow_traveller/SearchAndBook/SearchResultsActivity.java
+++ app/src/main/java/com/example/fellow_traveller/SearchAndBook/SearchResultsActivity.java
@@ -283,4 +283,18 @@ public class SearchResultsActivity extends AppCompatActivity {
         task.execute();
 
     }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == 1) {
+            if (resultCode == RESULT_OK) {
+                int result = data.getIntExtra("result", 0);
+
+            }
+            if (resultCode == RESULT_CANCELED) {
+
+            }
+        }
+    }
 }
