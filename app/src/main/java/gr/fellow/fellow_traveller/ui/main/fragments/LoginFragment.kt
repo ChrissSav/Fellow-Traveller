78536dc71a9c708f193d58f304b0a41e3096a95b
ChrissSav <dai17023@uom.edu.gr>
2020-07-28 22:18:27 +0300

    Hide keyboard on login

--- app/src/main/java/gr/fellow/fellow_traveller/ui/main/fragments/LoginFragment.kt
+++ app/src/main/java/gr/fellow/fellow_traveller/ui/main/fragments/LoginFragment.kt
@@ -1,23 +1,30 @@
 package gr.fellow.fellow_traveller.ui.main.fragments
 
+import android.app.Activity
+import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.text.Editable
+import android.text.InputType
+import android.text.TextWatcher
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.inputmethod.InputMethodManager
+import androidx.core.content.ContextCompat.getSystemService
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
-import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
 import androidx.navigation.NavController
 import androidx.navigation.Navigation
-import dagger.hilt.android.AndroidEntryPoint
 import dagger.hilt.android.WithFragmentBindings
 import gr.fellow.fellow_traveller.R
 import gr.fellow.fellow_traveller.databinding.FragmentLoginBinding
 import gr.fellow.fellow_traveller.ui.createSnackBar
 import gr.fellow.fellow_traveller.ui.home.main.HomeActivity
 import gr.fellow.fellow_traveller.ui.main.LoginViewModel
+import gr.fellow.fellow_traveller.ui.register.RegisterActivity
+
 
 @WithFragmentBindings
 class LoginFragment : Fragment() {
@@ -48,6 +55,7 @@ class LoginFragment : Fragment() {
 
 
         binding.buttonLogin.setOnClickListener {
+            context?.let { it1 -> hideKeyboardFrom(it1, binding.root) }
             if (binding.emailEditText.text.isEmpty() && binding.passwordEditText.text.isEmpty()) {
                 createSnackBar(view, resources.getString(R.string.ERROR_FIELDS_REQUIRE))
             } else {
@@ -64,10 +72,50 @@ class LoginFragment : Fragment() {
             startActivity(intent)
         })
 
-        binding.registerButton.setOnClickListener {
+        binding.passwordEditText.addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(charSequence: Editable?) {
+                if (charSequence.toString().trim().isNotEmpty())
+                    binding.displayPassword.visibility = View.VISIBLE
+                else
+                    binding.displayPassword.visibility = View.INVISIBLE
+            }
+
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+
+            }
+        })
+
+        binding.displayPassword.setOnClickListener {
+            if (binding.passwordEditText.inputType != InputType.TYPE_TEXT_VARIATION_VISIBLE_PASSWORD) {
+                binding.passwordEditText.inputType = InputType.TYPE_TEXT_VARIATION_VISIBLE_PASSWORD
+            } else {
+                binding.passwordEditText.inputType =
+                    InputType.TYPE_CLASS_TEXT or InputType.TYPE_TEXT_VARIATION_PASSWORD
+
+
+            }
+            binding.passwordEditText.setSelection(binding.passwordEditText.length())
 
         }
+
+        binding.registerButton.setOnClickListener {
+            val intent = Intent(activity, RegisterActivity::class.java)
+            startActivity(intent)
+        }
     }
+
+
+    private  fun hideKeyboardFrom(context: Context, view: View) {
+        val imm =
+            context.getSystemService(Activity.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.hideSoftInputFromWindow(view.windowToken, 0)
+    }
+
+
 
     override fun onDestroyView() {
         super.onDestroyView()
