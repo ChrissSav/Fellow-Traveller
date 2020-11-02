62f52847ead32cf576111345c43e3a642f7fee7d
ChrissSav <dai17023@uom.edu.gr>
2020-11-03 01:01:21 +0200

    [Search Trip] Update layout in SearchResultsAdapter.kt item

--- app/src/main/java/gr/fellow/fellow_traveller/ui/search/adapter/SearchResultsAdapter.kt
+++ app/src/main/java/gr/fellow/fellow_traveller/ui/search/adapter/SearchResultsAdapter.kt
@@ -33,7 +33,9 @@ class SearchResultsAdapter(
             binding.picture.loadImageFromUrl(currentTrip.creatorUser.picture)
             binding.rate.text = currentTrip.creatorUser.rate.toString()
             binding.review.text = currentTrip.creatorUser.reviews.toString()
-
+            binding.seats.text = currentTrip.seatsStatus
+            binding.bags.text = currentTrip.bags
+            binding.pet.text = if (currentTrip.hasPet) binding.pet.resources.getString(R.string.allowed) else binding.pet.resources.getString(R.string.not_allowed)
             binding.root.setOnClickListener {
                 onTripClickListener.invoke(currentTrip)
             }
