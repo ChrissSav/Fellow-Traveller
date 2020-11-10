19b0c35871c32d79486080ae1e9636c7bb3b70cf
Spuros <spurakos98@gmail.com>
2020-11-10 16:27:30 +0200

    [Filters] Set Range Material Date Picker

--- app/src/main/java/gr/fellow/fellow_traveller/ui/search/SearchFilterActivity.kt
+++ app/src/main/java/gr/fellow/fellow_traveller/ui/search/SearchFilterActivity.kt
@@ -2,12 +2,17 @@ package gr.fellow.fellow_traveller.ui.search
 
 import android.app.Activity
 import android.content.Intent
+import com.google.android.material.datepicker.MaterialDatePicker
 import gr.fellow.fellow_traveller.R
 import gr.fellow.fellow_traveller.data.base.BaseActivity
 import gr.fellow.fellow_traveller.databinding.ActivitySearchFilterBinding
 import gr.fellow.fellow_traveller.domain.PetAnswerType
 import gr.fellow.fellow_traveller.domain.SearchTripFilter
 import gr.fellow.fellow_traveller.ui.dialogs.bottom_sheet.SearchTripPetBottomSheetDialog
+import gr.fellow.fellow_traveller.ui.extensions.createToast
+import kotlinx.android.synthetic.main.search_trip_filter_bottom_sheet_dialog.*
+import java.text.SimpleDateFormat
+import java.util.*
 
 
 class SearchFilterActivity : BaseActivity<ActivitySearchFilterBinding>() {
@@ -15,6 +20,8 @@ class SearchFilterActivity : BaseActivity<ActivitySearchFilterBinding>() {
 
     private lateinit var petBottomSheetDialog: SearchTripPetBottomSheetDialog
     private lateinit var searchTripFilter: SearchTripFilter
+
+
 
     override fun provideViewBinding(): ActivitySearchFilterBinding =
         ActivitySearchFilterBinding.inflate(layoutInflater)
@@ -44,9 +51,20 @@ class SearchFilterActivity : BaseActivity<ActivitySearchFilterBinding>() {
             seatsPickButton.currentNum = searchTripFilter.seatsMin ?: 1
             initializePet()
 
+            /** Date Picker Initialization **/
+            val builder = MaterialDatePicker.Builder.dateRangePicker()
+            val now = Calendar.getInstance()
+            builder.setSelection(androidx.core.util.Pair(now.timeInMillis, now.timeInMillis))
+            builder.setTitleText("Επιλέξτε εύρος ημερομηνίας για αναζήτηση")
+            val picker = builder.build()
+
             /** OnClicks **/
 
+            date.setOnClickListener {
+                picker.show(this@SearchFilterActivity?.supportFragmentManager!!, picker.toString())
 
+
+            }
             pet.setOnClickListener {
                 petBottomSheetDialog = SearchTripPetBottomSheetDialog(this@SearchFilterActivity::onPetItemClickListener)
                 petBottomSheetDialog.show(supportFragmentManager, "petBottomSheetDialog")
@@ -69,6 +87,24 @@ class SearchFilterActivity : BaseActivity<ActivitySearchFilterBinding>() {
 
 
             /** Handle Changes **/
+
+            //Cancel button on dialog
+            picker.addOnNegativeButtonClickListener {
+                builder.setSelection(androidx.core.util.Pair(now.timeInMillis, now.timeInMillis))
+                builder.build()
+                //Long first/second to null
+                date.setText("Εύρος ημ/νίας")}
+
+            picker.addOnPositiveButtonClickListener {
+                //"The selected date range it  ${it.first} - ${it.second}"
+                //Convert Long to Date Format
+                date.setText(it.first?.let { it1 -> convertLongToTime(it1) } + " - " + it.second?.let { it1 -> convertLongToTime(it1) }) }
+
+            picker.addOnCancelListener { }
+            picker.addOnDismissListener { }
+
+
+
 
             fromRangeSeekbar.setOnSeekbarChangeListener { value ->
                 if (value.toInt() == 0) {
@@ -103,6 +139,11 @@ class SearchFilterActivity : BaseActivity<ActivitySearchFilterBinding>() {
         }
     }
 
+    fun convertLongToTime(time: Long): String {
+        val date = Date(time)
+        val format = SimpleDateFormat("dd MMM")
+        return format.format(date)
+    }
     private fun applyChanges() {
         val resultIntent = Intent()
         if (binding.seatsPickButton.currentNum != 1)
@@ -137,4 +178,5 @@ class SearchFilterActivity : BaseActivity<ActivitySearchFilterBinding>() {
             }
         }
     }
+
 }
