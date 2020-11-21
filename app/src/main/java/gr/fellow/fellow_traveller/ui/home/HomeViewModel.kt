df87e472ba42f57dfde886102850f6080950b88f
ChrissSav <dai17023@uom.edu.gr>
2020-11-21 14:47:22 +0200

    [Notification] Fix bug read notification

--- app/src/main/java/gr/fellow/fellow_traveller/ui/home/HomeViewModel.kt
+++ app/src/main/java/gr/fellow/fellow_traveller/ui/home/HomeViewModel.kt
@@ -286,12 +286,14 @@ constructor(
 
     fun readNotification(notification: Notification) {
         launchWithLiveData(false, loadNotifications) {
-            updateNotificationsUseCase(notification.id)
-            notification.isRead = true
-            val temp = _notifications.value ?: mutableListOf()
-            val index = temp.indexOf(notification)
-            temp[index] = notification
-            _notifications.value = temp
+            if (!notification.isRead) {
+                updateNotificationsUseCase(notification.id)
+                notification.isRead = true
+                val temp = _notifications.value ?: mutableListOf()
+                val index = temp.indexOf(notification)
+                temp[index] = notification
+                _notifications.value = temp
+            }
             _notification.value = notification
         }
     }
