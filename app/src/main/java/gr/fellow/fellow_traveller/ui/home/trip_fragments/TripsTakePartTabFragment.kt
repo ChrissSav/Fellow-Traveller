b3e69cad717201edca9c7a310fde1015e42aec8f
ChrissSav <dai17023@uom.edu.gr>
2020-11-26 19:08:44 +0200

    [Home] Fix bug in Activity search and add trip

--- app/src/main/java/gr/fellow/fellow_traveller/ui/home/trip_fragments/TripsTakePartTabFragment.kt
+++ app/src/main/java/gr/fellow/fellow_traveller/ui/home/trip_fragments/TripsTakePartTabFragment.kt
@@ -1,5 +1,7 @@
 package gr.fellow.fellow_traveller.ui.home.trip_fragments
 
+import android.app.Activity
+import android.content.Intent
 import android.view.View
 import androidx.core.os.bundleOf
 import androidx.fragment.app.activityViewModels
@@ -11,8 +13,10 @@ import gr.fellow.fellow_traveller.databinding.FragmentTakesPartTabBinding
 import gr.fellow.fellow_traveller.domain.trip.TripInvolved
 import gr.fellow.fellow_traveller.ui.extensions.createAlerter
 import gr.fellow.fellow_traveller.ui.extensions.findNavController
+import gr.fellow.fellow_traveller.ui.extensions.startActivityForResult
 import gr.fellow.fellow_traveller.ui.home.HomeViewModel
 import gr.fellow.fellow_traveller.ui.home.adapter.TripsInvolvedAdapter
+import gr.fellow.fellow_traveller.ui.search.SearchTripActivity
 
 
 @AndroidEntryPoint
@@ -66,7 +70,7 @@ class TripsTakePartTabFragment : BaseFragment<FragmentTakesPartTabBinding>() {
 
         binding.searchButton.setOnClickListener {
             if (accountCorrect)
-                findNavController()?.navigate(R.id.action_destination_trips_to_searchTripActivity)
+                startActivityForResult(SearchTripActivity::class, 2, null)
             else
                 createAlerter(getString(R.string.complete_profile_warning))
         }
@@ -82,5 +86,18 @@ class TripsTakePartTabFragment : BaseFragment<FragmentTakesPartTabBinding>() {
         findNavController()?.navigate(R.id.action_destination_trips_to_tripInvolvedDetailsFragment, bundleOf("tripId" to tripInvolved.id))
     }
 
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == 2) {
+            if (resultCode == Activity.RESULT_OK) {
+                val trip = data?.getParcelableExtra<TripInvolved>("trip")
+                trip?.let {
+                    viewModel.addTripPassenger(it)
+                }
+            }
+
+        }
+    }
 
 }
