7122b086217cf03ce7f7ac5a04ed97c0dc982713
ChrissSav <dai17023@uom.edu.gr>
2020-12-06 23:49:41 +0200

    [Splash] Fix some bugs in splash gif

--- app/src/main/java/gr/fellow/fellow_traveller/ui/splash/SplashActivity.kt
+++ app/src/main/java/gr/fellow/fellow_traveller/ui/splash/SplashActivity.kt
@@ -2,13 +2,20 @@ package gr.fellow.fellow_traveller.ui.splash
 
 import android.content.Intent
 import android.content.SharedPreferences
+import android.graphics.drawable.Drawable
 import androidx.activity.viewModels
+import androidx.core.content.ContextCompat
 import androidx.lifecycle.Observer
+import androidx.vectordrawable.graphics.drawable.Animatable2Compat
 import com.bumptech.glide.Glide
+import com.bumptech.glide.load.DataSource
+import com.bumptech.glide.load.engine.GlideException
+import com.bumptech.glide.load.resource.gif.GifDrawable
+import com.bumptech.glide.request.RequestListener
+import com.bumptech.glide.request.target.Target
 import dagger.hilt.android.AndroidEntryPoint
 import gr.fellow.fellow_traveller.R
 import gr.fellow.fellow_traveller.data.base.BaseActivity
-import gr.fellow.fellow_traveller.data.base.GifDrawableImageViewTarget
 import gr.fellow.fellow_traveller.databinding.ActivitySplashBinding
 import gr.fellow.fellow_traveller.ui.extensions.openActivityWithFade
 import gr.fellow.fellow_traveller.ui.extensions.postDelay
@@ -55,8 +62,26 @@ class SplashActivity : BaseActivity<ActivitySplashBinding>() {
             viewModel.setSecond(true)
         }
 
-        Glide.with(this)
-            .load(R.raw.splash_green_100fps)
-            .into(GifDrawableImageViewTarget(binding.ImageView, 1))
+        Glide.with(this).asGif().load(R.raw.splash_green_100fps).listener(object : RequestListener<GifDrawable?> {
+
+            override fun onLoadFailed(e: GlideException?, model: Any?, target: Target<GifDrawable?>?, isFirstResource: Boolean): Boolean {
+                binding.ImageView.setColorFilter(ContextCompat.getColor(this@SplashActivity, R.color.white), android.graphics.PorterDuff.Mode.SRC_IN);
+                binding.ImageView.setImageResource(R.drawable.ic_fellow);
+                return true
+            }
+
+            override fun onResourceReady(
+                resource: GifDrawable?, model: Any?, target: Target<GifDrawable?>?, dataSource: DataSource?, isFirstResource: Boolean
+            ): Boolean {
+                resource?.setLoopCount(1)
+                resource?.registerAnimationCallback(object : Animatable2Compat.AnimationCallback() {
+                    override fun onAnimationEnd(drawable: Drawable) {
+
+                    }
+                })
+                return false
+            }
+        }).override(Target.SIZE_ORIGINAL, Target.SIZE_ORIGINAL)
+            .into(binding.ImageView)
     }
 }
