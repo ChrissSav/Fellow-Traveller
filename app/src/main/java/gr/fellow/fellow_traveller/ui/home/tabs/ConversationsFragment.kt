94d884ee9ea8f9a6092f1c9de8a6a580f16dc424
Spuros <spurakos98@gmail.com>
2020-12-17 21:36:25 +0200

    [Messenger] Layouts

--- app/src/main/java/gr/fellow/fellow_traveller/ui/home/tabs/ConversationsFragment.kt
+++ app/src/main/java/gr/fellow/fellow_traveller/ui/home/tabs/ConversationsFragment.kt
@@ -1,23 +1,21 @@
 package gr.fellow.fellow_traveller.ui.home.tabs
-
 import androidx.fragment.app.activityViewModels
+import dagger.hilt.android.AndroidEntryPoint
 import gr.fellow.fellow_traveller.data.base.BaseFragment
-import gr.fellow.fellow_traveller.databinding.FragmentMessengerBinding
-import gr.fellow.fellow_traveller.ui.extensions.createToast
+import gr.fellow.fellow_traveller.databinding.FragmentConversationsBinding
 import gr.fellow.fellow_traveller.ui.home.HomeViewModel
 import gr.fellow.fellow_traveller.ui.home.adapter.ConversationsAdapter
 import gr.fellow.fellow_traveller.ui.home.chat.models.Conversation
 
-
-class MessengerFragment : BaseFragment<FragmentMessengerBinding>() {
+@AndroidEntryPoint
+class ConversationsFragment : BaseFragment<FragmentConversationsBinding>() {
     private val conversationsList = mutableListOf<Conversation>()
 
     private val viewModel: HomeViewModel by activityViewModels()
 
 
-    override fun getViewBinding(): FragmentMessengerBinding =
-        FragmentMessengerBinding.inflate(layoutInflater)
-
+    override fun getViewBinding(): FragmentConversationsBinding =
+        FragmentConversationsBinding.inflate(layoutInflater)
 
 
     override fun setUpObservers() {
@@ -27,7 +25,7 @@ class MessengerFragment : BaseFragment<FragmentMessengerBinding>() {
 
     override fun setUpViews() {
 
-        conversationsList.add(Conversation("ufsdfsf2432", "Φιλώτας-Πτολεμαΐδα", "Ναιι θα περάσω το πδασδασδασδασδαδασφασφαφσαφσαφαφαασφασφαρωί αν είναι", 1608218840, "default", false))
+        conversationsList.add(Conversation("ufsdfsf2432", "Φιλώτας-Πτολεμαΐδα", "Ναιι θα περάσω το παρωί αν είναι", 1608218840, "default", false))
         conversationsList.add(Conversation("ufsdfsf2432", "Φιλώτας-Πτολεμαΐδα", "Ναιι θα περάσω το πρωί αν είναι", 1608218126, "default", false))
         conversationsList.add(Conversation("ufsdfsf2432", "Φιλώτας-Πτολεμαΐδα", "Ναιι θα περάσω το πρωί αν είναι", 1608218126, "default", false))
         conversationsList.add(Conversation("ufsdfsf2432", "Φιλώτας-Πτολεμαΐδα", "Ναιι θα περάσω το πρωί αν είναι", 1608218126, "default", false))
@@ -42,7 +40,7 @@ class MessengerFragment : BaseFragment<FragmentMessengerBinding>() {
         binding.recyclerView.adapter = ConversationsAdapter(conversationsList)
 
         //binding.recyclerView.adapter?.notifyDataSetChanged()
-        createToast(conversationsList.get(0).description)
+
     }
 
 
